ad1abe6335751798f4ad46c288c5135a38191dbb
Creiwry <jay.gardos@outlook.com>
2023-12-06 14:46:15 +0100

    feat(db): add associations to models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,5 @@
 class Invoice < ApplicationRecord
   belongs_to :user
   belongs_to :admin, class_name: 'User', foreign_key: 'admin_id'
+  has_many :appointments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   devise :database_authenticatable, :registerable,
          :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_many :invoices
+  has_many :appointments, through: :invoices
+
+  has_many :invoices, foreign_key: 'admin_id'
+  has_many :appointments, through: :invoices, foreign_key: 'admin_id'
+
+  has_one :cart
+  has_many :cart_items, through: :cart
+
+  has_many :updates
+
+  has_many :orders
+  has_many :order_items, through: :orders
 end
