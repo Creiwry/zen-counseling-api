b8cdc5290b2301b05a60f83d436487b236973f0e
Creiwry <jay.gardos@outlook.com>
2023-12-11 17:38:16 +0100

    fix(models):remove username validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_06_131717) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_06_130951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -136,7 +136,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_06_131717) do
     t.boolean "admin", default: false
     t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
