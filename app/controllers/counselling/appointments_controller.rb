22cebad299faa799b8059fa048c8aa79ddcfea61
Creiwry <jay.gardos@outlook.com>
2023-12-12 23:15:37 +0100

    feat(controllers): add authentication to appointments controller

--- app/controllers/counselling/appointments_controller.rb
+++ app/controllers/counselling/appointments_controller.rb
@@ -1,49 +1,70 @@
 class Counselling::AppointmentsController < ApplicationController
-  before_action :set_appointment, only: %i[ show update destroy ]
+  before_action :set_appointment, only: %i[show update destroy]
   before_action :authenticate_user!
 
   # GET /appointments
   def index
-    @appointments = Appointment.all
+    appointments = current_user.admin ? Appointment.where(admin: current_user) : Appointment.where(client: current_user)
 
-    render_response(200, 'index rendered', :ok, @appointments)
+    if params[:date] != ''
+      date = params[:date].to_date
+      appointments = appointments.filter_based_on_date(date)
+    end
+    render_response(200, 'index rendered', :ok, appointments)
   end
 
   # GET /appointments/1
   def show
-    render_response(200, 'show appointment', :ok, @appointment)
+    if current_user == @appointment.admin || current_user == @appointment.client
+      render_response(200, 'show appointment', :ok, @appointment)
+    else
+      render_response(401, 'You are not authorized to create this resource', :unauthorized, nil)
+    end
   end
 
   # POST /appointments
   def create
-    @appointment = Appointment.new(appointment_params)
-    user = User.find(params[:user_id])
     invoice = Invoice.find(params[:invoice_id])
+    if check_admin(invoice.admin) || check_client(invoice.client)
+      client = User.find(params[:user_id])
+      @appointment = Appointment.new(
+        appointment_params,
+        admin: current_user,
+        client:,
+        invoice:
+      )
 
-    @appointment.admin = current_user
-    @appointment.client = user
-    @appointment.invoice = invoice
-
-    if @appointment.save
-      render_response(201, 'Appointment created', :created, @appointment)
+      if @appointment.save
+        render_response(201, 'Appointment created', :created, @appointment)
+      else
+        render_response(422, @appointment.errors, :unprocessable_entity, nil)
+      end
     else
-      render_response(422, @appointment.errors, :unprocessable_entity, nil)
+      render_response(401, 'You are not authorized to create this resource', :unauthorized, nil)
     end
   end
 
   # PATCH/PUT /appointments/1
   def update
-    if @appointment.update(appointment_params)
-      render_response(200, 'resource updated successfully', :ok, @appointment)
+    if check_admin(current_user) || check_client(current_user)
+      if @appointment.update(appointment_params)
+        render_response(200, 'resource updated successfully', :ok, @appointment)
+      else
+        render_response(422, @appointment.errors, :unprocessable_entity, @appointment)
+      end
     else
-      render_response(422, @appointment.errors, :unprocessable_entity, @appointment)
+      render_response(401, 'You are not authorized to change this resource', :unauthorized, nil)
     end
   end
 
   # DELETE /appointments/1
   def destroy
-    @appointment.destroy!
-    render_response(200, 'resource deleted successfully', :ok, nil)
+    if check_admin(current_user) || check_client(current_user)
+      @appointment.destroy!
+      render_response(200, 'resource deleted successfully', :ok, nil)
+    else
+      render_response(401, 'You are not authorized to change this resource', :unauthorized, nil)
+    end
   end
 
   private
@@ -53,27 +74,16 @@ class Counselling::AppointmentsController < ApplicationController
     @appointment = Appointment.find(params[:id])
   end
 
-  def check_current_user_admin
+  def check_admin(user)
     appointment = Appointment.find(params[:id])
-    return if current_user == appointment.admin
-
-    render json: {
-      status: {
-        code: 401, errors: 'You are not authorized to change this appointment'
-      }
-    }, status: :unauthorized
+    user == appointment.admin
   end
 
-  def check_current_user_client
+  def check_client(user)
     appointment = Appointment.find(params[:id])
-    return if current_user == appointment.client
-
-    render json: {
-      status: {
-        code: 401, errors: 'You are not authorized to change this appointment'
-      }
-    }, status: :unauthorized
+    user == appointment.client
   end
+
   # Only allow a list of trusted parameters through.
   def appointment_params
     params.require(:appointment).permit(:invoice_id, :user_id, :admin_id, :datetime, :appointment_type, :link, :status)
