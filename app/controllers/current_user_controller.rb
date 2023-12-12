cd7ec18a5dd64bf8561c6955330027380c0e5873
Creiwry <jay.gardos@outlook.com>
2023-12-12 14:15:46 +0100

    fix(controllers): add admin attribute to current_user

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -4,8 +4,4 @@ class CurrentUserController < ApplicationController
   def index
     render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
   end
-
-  def is_admin
-    render json: { admin: current_user.admin }, status: :ok 
-  end
 end
