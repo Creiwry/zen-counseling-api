c902d6d6b5deb400df793f7f998da193436e8981
Creiwry <jay.gardos@outlook.com>
2023-12-13 00:06:00 +0100

    refactor(controllers): standardize response for all controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,16 @@
 class UsersController < ApplicationController
-    before_action :check_if_admin, only: %i[index]
-  
-    # GET /updates
-    def index
-      @users = User.where.not(id: current_user.id)
-  
-      render json: @users
-    end
-  
-  
-    def check_if_admin
-      return if current_user.admin
-  
-      render json: { status: 401, message: "You are not authorized to access this resource" }, status: 401
-    end
-   
+  before_action :check_if_admin, only: %i[index]
+
+  # GET /updates
+  def index
+    @users = User.where.not(id: current_user.id)
+
+    render_response(200, 'index users successful', :ok, @users)
   end
-  
+
+  def check_if_admin
+    return if current_user.admin
+
+    render_response(401, 'You are not authorized to access this resource', :unauthorized, nil)
+  end
+end
