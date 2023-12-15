4ca590f4406a14ae5c975864bd266a4d771c30c3
Creiwry <jay.gardos@outlook.com>
2023-12-15 18:28:38 +0100

    fix(controllers): switch invoice to url

--- app/controllers/counselling/invoices_controller.rb
+++ app/controllers/counselling/invoices_controller.rb
@@ -30,7 +30,7 @@ class Counselling::InvoicesController < ApplicationController
     invoice = Invoice.find(params[:invoice_id])
     invoice_url = url_for(invoice.document)
 
-    render_response(200, 'resource found', :ok, invoice.document)
+    render_response(200, 'resource found', :ok, invoice_url)
   end
   # POST /invoices
   def create
