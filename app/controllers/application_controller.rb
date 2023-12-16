a523128d20fa5d1ede0cd3d31c32b0c598d0544e
Creiwry <jay.gardos@outlook.com>
2023-12-16 23:28:37 +0100

    fix(error): fix error handling in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::API
   before_action :configure_devise_parameters, if: :devise_controller?
 
   rescue_from ActionDispatch::Http::Parameters::ParseError do
-    render json: { status: 400, message: 'Bad Request: Error occurred while parsing request parameters' }
+    render json: { status: 400, message: 'Bad Request: Error occurred while parsing request parameters' }, status: :bad_request
   end
 
   rescue_from ActiveRecord::RecordNotFound do
-    render json: { status: :not_found, message: 'This record was not found' }
+    render json: { status: :not_found, message: 'This record was not found' }, status: :not_found
   end
 
   def render_response(code, message, status, data)
