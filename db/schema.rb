4fdaf22a316c441aa7650c4fbc136a3807101e70
Creiwry <jay.gardos@outlook.com>
2024-01-12 17:28:36 +0100

    feat(models): add full name to User

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,163 +10,163 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_231_214_132_941) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_12_161704) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
-                                                    unique: true
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum'
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'appointments', force: :cascade do |t|
-    t.bigint 'invoice_id', null: false
-    t.bigint 'client_id', null: false
-    t.bigint 'admin_id', null: false
-    t.datetime 'datetime'
-    t.string 'link'
-    t.string 'status'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'appointment_type'
-    t.index ['admin_id'], name: 'index_appointments_on_admin_id'
-    t.index ['client_id'], name: 'index_appointments_on_client_id'
-    t.index ['invoice_id'], name: 'index_appointments_on_invoice_id'
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "invoice_id", null: false
+    t.bigint "client_id", null: false
+    t.bigint "admin_id", null: false
+    t.datetime "datetime"
+    t.string "link"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "appointment_type"
+    t.index ["admin_id"], name: "index_appointments_on_admin_id"
+    t.index ["client_id"], name: "index_appointments_on_client_id"
+    t.index ["invoice_id"], name: "index_appointments_on_invoice_id"
   end
 
-  create_table 'cart_items', force: :cascade do |t|
-    t.bigint 'cart_id', null: false
-    t.bigint 'item_id', null: false
-    t.integer 'quantity'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['cart_id'], name: 'index_cart_items_on_cart_id'
-    t.index ['item_id'], name: 'index_cart_items_on_item_id'
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "item_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
 
-  create_table 'carts', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_carts_on_user_id'
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
-  create_table 'invoices', force: :cascade do |t|
-    t.integer 'appointment_number'
-    t.decimal 'total', precision: 10, scale: 2
-    t.string 'status'
-    t.bigint 'client_id', null: false
-    t.bigint 'admin_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'stripe_customer_id'
-    t.index ['admin_id'], name: 'index_invoices_on_admin_id'
-    t.index ['client_id'], name: 'index_invoices_on_client_id'
+  create_table "invoices", force: :cascade do |t|
+    t.integer "appointment_number"
+    t.decimal "total", precision: 10, scale: 2
+    t.string "status"
+    t.bigint "client_id", null: false
+    t.bigint "admin_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "stripe_customer_id"
+    t.index ["admin_id"], name: "index_invoices_on_admin_id"
+    t.index ["client_id"], name: "index_invoices_on_client_id"
   end
 
-  create_table 'items', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.decimal 'price', precision: 10, scale: 2
-    t.integer 'stock'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price", precision: 10, scale: 2
+    t.integer "stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'order_items', force: :cascade do |t|
-    t.bigint 'order_id', null: false
-    t.bigint 'item_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['item_id'], name: 'index_order_items_on_item_id'
-    t.index ['order_id'], name: 'index_order_items_on_order_id'
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
-  create_table 'orders', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.string 'stripe_customer_id'
-    t.decimal 'total', precision: 10, scale: 2
-    t.string 'address'
-    t.string 'status'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_orders_on_user_id'
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "stripe_customer_id"
+    t.decimal "total", precision: 10, scale: 2
+    t.string "address"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table 'private_messages', force: :cascade do |t|
-    t.text 'content'
-    t.bigint 'recipient_id'
-    t.bigint 'sender_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['recipient_id'], name: 'index_private_messages_on_recipient_id'
-    t.index ['sender_id'], name: 'index_private_messages_on_sender_id'
+  create_table "private_messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "recipient_id"
+    t.bigint "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_private_messages_on_recipient_id"
+    t.index ["sender_id"], name: "index_private_messages_on_sender_id"
   end
 
-  create_table 'updates', force: :cascade do |t|
-    t.string 'title'
-    t.text 'content'
-    t.bigint 'admin_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['admin_id'], name: 'index_updates_on_admin_id'
+  create_table "updates", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "admin_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_updates_on_admin_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'jti', null: false
-    t.boolean 'admin', default: false
-    t.string 'first_name', null: false
-    t.string 'last_name', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['jti'], name: 'index_users_on_jti', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "jti", null: false
+    t.boolean "admin", default: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "full_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'appointments', 'invoices'
-  add_foreign_key 'appointments', 'users', column: 'admin_id'
-  add_foreign_key 'appointments', 'users', column: 'client_id'
-  add_foreign_key 'cart_items', 'carts'
-  add_foreign_key 'cart_items', 'items'
-  add_foreign_key 'carts', 'users'
-  add_foreign_key 'invoices', 'users', column: 'admin_id'
-  add_foreign_key 'invoices', 'users', column: 'client_id'
-  add_foreign_key 'order_items', 'items'
-  add_foreign_key 'order_items', 'orders'
-  add_foreign_key 'orders', 'users'
-  add_foreign_key 'private_messages', 'users', column: 'recipient_id', on_delete: :cascade
-  add_foreign_key 'private_messages', 'users', column: 'sender_id', on_delete: :cascade
-  add_foreign_key 'updates', 'users', column: 'admin_id'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "appointments", "invoices"
+  add_foreign_key "appointments", "users", column: "admin_id"
+  add_foreign_key "appointments", "users", column: "client_id"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "items"
+  add_foreign_key "carts", "users"
+  add_foreign_key "invoices", "users", column: "admin_id"
+  add_foreign_key "invoices", "users", column: "client_id"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "users"
+  add_foreign_key "private_messages", "users", column: "recipient_id", on_delete: :cascade
+  add_foreign_key "private_messages", "users", column: "sender_id", on_delete: :cascade
+  add_foreign_key "updates", "users", column: "admin_id"
 end
